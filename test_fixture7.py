dc49f20c6c46fa5053c3835e2591496b4ffca79a
Elly806 <84780463+Elly806@users.noreply.github.com>
2021-05-27 17:00:31 +0200

    Update test_fixture7.py

--- test_fixture7.py
+++ test_fixture7.py
@@ -11,6 +11,6 @@ def browser():
 
 @pytest.mark.parametrize('language', ["ru", "en-gb"])
 def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    link = f"https://my.zanichelli.it//{language}/"
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
